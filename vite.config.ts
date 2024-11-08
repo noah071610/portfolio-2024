53d958ddd60dc3e597f5e5a156530f86212983af
Noah Jang <noah071610@gmail.com>
2024-11-08 10:38:42 +0900

    fix / vite deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
